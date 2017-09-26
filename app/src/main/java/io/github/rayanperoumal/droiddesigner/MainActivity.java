df7a7dbd1843dceea242f910cc8d17e6f8a3b568
rayanperoumal <rayan.peroumal@gmail.com>
2017-09-26 12:57:33 -0400

    Test Git Clone : Back to jgit 3.7.0 and apache lib for httpd client

--- app/src/main/java/io/github/rayanperoumal/droiddesigner/MainActivity.java
+++ app/src/main/java/io/github/rayanperoumal/droiddesigner/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.rayanperoumal.droiddesigner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File f = new File(getFilesDir(),"/repo8/");
+        f.mkdirs();
+        try {
+            Git git = Git.cloneRepository()
+                    .setURI( "https://github.com/rayanperoumal/droiddesigner.git" )
+                    .setDirectory(f)
+                    .call();
+        } catch (GitAPIException e) {
+            e.printStackTrace();
+        }
+
     }
 }
