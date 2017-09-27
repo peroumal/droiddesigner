8599167141596a319dac78ea3e1ec67315757256
rayanperoumal <rayan.peroumal@gmail.com>
2017-09-27 11:02:47 -0400

    Check format of the domain & repo name of an given repository url

--- app/src/main/java/io/github/rayanperoumal/droiddesigner/MainActivity.java
+++ app/src/main/java/io/github/rayanperoumal/droiddesigner/MainActivity.java
@@ -2,6 +2,15 @@ package io.github.rayanperoumal.droiddesigner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -10,11 +19,19 @@ import java.io.File;
 import io.github.rayanperoumal.droiddesigner.git.Clone;
 
 public class MainActivity extends AppCompatActivity {
-
+    CardView clonerView;
+    EditText cloneText;
+    TextView cloneTitle,alert;
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clonerView = (CardView) findViewById(R.id.clone);
+        cloneText = (EditText) findViewById(R.id.cloneText);
+        cloneTitle = (TextView) findViewById(R.id.cloneTitle);
+        alert = (TextView) findViewById(R.id.alert);
+        submit = (Button)findViewById(R.id.submit);
         new Clone("https://github.com/rayanperoumal/droiddesigner.git"){
             @Override
             protected void onPostExecute(Integer integer) {
@@ -22,5 +39,61 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this,getMessage(integer),Toast.LENGTH_LONG).show();
             }
         }.execute(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        clonerView.setOnClickListener(view -> {
+                cloneText.setEnabled(true);
+                cloneText.setHint("domaine/repertoire/repository.git");
+                cloneTitle.setVisibility(View.VISIBLE);
+                cloneTitle.setText("Specifier le nom du repository");
+                Log.i("activity:event","clone:called");
+        });
+
+        cloneText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(charSequence.toString().endsWith(".git")){
+                    if(checkDomain(charSequence.toString())){
+                        alert.setVisibility(View.VISIBLE);
+                        alert.setText("Le repo est pret à etre cloné");
+                        submit.setVisibility(View.VISIBLE);
+                    }
+                    else{
+                        submit.setVisibility(View.GONE);
+                        alert.setVisibility(View.VISIBLE);
+                        alert.setText("Ce repository ne contient pas de domaine valide");
+                    }
+                } else {
+                    submit.setVisibility(View.GONE);
+                    alert.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+    }
+    public boolean checkDomain(String name){
+        boolean domain=false;
+        int slash=-1, point=-1;
+        for(int i=0; i<(name.length()-5) && slash<0;i++)
+            switch(name.charAt(i)){
+                case '/': slash=i;break;
+                case '.': point=i;break;
+            }
+        if((point+1)<slash) domain = true;
+        return domain;
     }
 }
